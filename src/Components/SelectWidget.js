1a68c1a7ac718c30d58d2d63aeaad2e02407d66b
Pradeepa yadav <pradeep90043@gmail.com>
2022-01-27 17:58:35 +0530

    m

--- src/Components/SelectWidget.js
+++ src/Components/SelectWidget.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 function SelectWidget(props) {
-    const color = props.background
-  return <div>
-      <p className='widgets' onClick={props.setBackground(props.background)} style={{backgroundColor:{color}}} >{props.color||props.size }  </p>
-  </div>;
+  const color = props.background;
+  useEffect(()=>props.setBackground(props.color||props.size),[color])
+  return (
+    <div>
+      <p
+        className="widgets"
+        onClick={props.setBackground(props.background)}
+        style={{ backgroundColor: { color } }}
+      >
+        {props.color || props.size}
+      </p>
+    </div>
+  );
 }
 
 export default SelectWidget;
